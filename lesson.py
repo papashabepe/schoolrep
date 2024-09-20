e5a53e853e16176772c29f325a6db6cc971bd77f
mihas <mihasokol428@gmail.com>
2024-09-20 20:02:17 +0300

    change class lesson was moved into lesson.py

--- lesson.py
+++ lesson.py
@@ -1,2 +1,8 @@
 class Lesson:
-    pass
+    def __init__(self, subject , classroom , time):
+        self.__subject = subject
+        self.__classroom = classroom
+        self.__time = time
+
+    def __str__(self):
+        return "|" + self.__time + "|" + self.__subject + "|" + self.__classroom + "|"
---
+++ run.py
@@ -0,0 +1,10 @@
+from constans import SUBJECT, CLASSROOMS , TIME
+from lesson import Lesson
+
+lesson = Lesson(SUBJECT[0] , CLASSROOMS[0] ,TIME[0])
+print(lesson)
+print(lesson)
+print(lesson)
+print(lesson)
+print(lesson)
+print(lesson)
