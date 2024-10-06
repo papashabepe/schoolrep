aebb52dd3ab7136e0bab01a1fd6b907267f76cfc
mihas <mihasokol428@gmail.com>
2024-10-06 18:53:13 +0300

    homework2

---
+++ snake.py
@@ -0,0 +1,5 @@
+class Snake:
+    def __init__(self, speed , length , direction , ):
+        self.speed = speed
+        self.length = length
+        self.direction = direction
---
+++ snakes_food.py
@@ -0,0 +1,3 @@
+class Food
+    def __init__(self, position):
+        self.position = position
